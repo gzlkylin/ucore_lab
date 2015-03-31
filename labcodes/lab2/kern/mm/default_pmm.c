33cbf6ade3d2b055dd5293feca66e5f8d77eb6db
gzlkylin <gzlkylin@gmail.com>
2015-03-31 15:47:44 +0800

    first fit completed

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -58,7 +58,10 @@ free_area_t free_area;
 
 #define free_list (free_area.free_list)
 #define nr_free (free_area.nr_free)
-
+#define show_list()  le = &free_list;  \
+	while ((le = list_next(le)) != &free_list) {\
+		cprintf("[addr]:%u ", le2page(le, page_link));\
+	}
 static void
 default_init(void) {
     list_init(&free_list);
@@ -73,6 +76,7 @@ default_init_memmap(struct Page *base, size_t n) {
         assert(PageReserved(p));
         p->flags = p->property = 0;
         set_page_ref(p, 0);
+		p->page_link.next = p->page_link.prev = &p->page_link;
     }
     base->property = n;
     SetPageProperty(base);
@@ -95,22 +99,23 @@ default_alloc_pages(size_t n) {
             break;
         }
     }
+	le = list_next(le);
     if (page != NULL) {
         list_del(&(page->page_link));
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+            list_add_before(le, &(p->page_link));
+    	}
         nr_free -= n;
         ClearPageProperty(page);
     }
+
     return page;
 }
 
 static void
 default_free_pages(struct Page *base, size_t n) {
-	cprintf("%d\n", nr_free);
     assert(n > 0);
     struct Page *p = base;
     for (; p != base + n; p ++) {
@@ -121,44 +126,38 @@ default_free_pages(struct Page *base, size_t n) {
     base->property = n;
     SetPageProperty(base);
     list_entry_t *le = &free_list;
-	
-
-	cprintf("1");
-	le = &free_list;
-	while ((le = list_next(le)) != &free_list) {
-		cprintf("[addr]:%d ", le2page(le, page_link) - base);
-	}
-	cprintf("1\n");
-
 
    	le = list_next(&free_list); 
     p = le2page(le, page_link);
 
-	while ((p - base < 0) && (le != &free_list)) {
+	while ((p - base < 0) && (le != &free_list)) {//find the correct address
 		le = list_next(le);
 		p = le2page(le, page_link);
-		cprintf("+\n");
 	}
-
-	if (base + base->property == p)
+	
+	le = list_prev(le);
+	p = le2page(le, page_link);
+	le = list_next(le);
+	if(p + p->property == base)//if the former can be combined
 	{
+		p->property += base->property;
+		base->property = 0;
+		ClearPageProperty(base);
+		base = p;
+		list_del(&(p->page_link));
+	}
+	p = le2page(le, page_link);
+	if (base + base->property == p)//if the after can be combined
+	{
+			le = list_next(le);
 			base->property += p->property;
            	ClearPageProperty(p);
+			p->property = 0;
            	list_del(&(p->page_link));
 	}
 	nr_free += n;
-	if (le == &free_list)
-	list_add_before(&free_list, &(base->page_link));	
-	else
 	list_add_before(le, &(base->page_link));
-	cprintf("2");
-	le = &free_list;
-	while ((le = list_next(le)) != &free_list) {
-		cprintf("[addr]:%d ", le2page(le, page_link) - base);
-	}
-	cprintf("2\n");
 }
-
 static size_t
 default_nr_free_pages(void) {
     return nr_free;
@@ -209,7 +208,6 @@ basic_check(void) {
     assert(nr_free == 0);
     free_list = free_list_store;
     nr_free = nr_free_store;
-
     free_page(p);
     free_page(p1);
     free_page(p2);
@@ -264,7 +262,7 @@ default_check(void) {
 
     assert((p0 = alloc_pages(5)) != NULL);
     assert(alloc_page() == NULL);
-
+	
     assert(nr_free == 0);
     nr_free = nr_free_store;
 
