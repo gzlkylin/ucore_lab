e21a185764fc95f56d6e178fcbb3434b04a5079b
gzlkylin <gzlkylin@gmail.com>
2015-04-10 19:08:05 +0800

    lab3

--- labcodes/lab3/kern/trap/trap.c
+++ labcodes/lab3/kern/trap/trap.c
@@ -29,6 +29,7 @@ static void print_ticks() {
  * */
 static struct gatedesc idt[256] = {{0}};
 
+extern uintptr_t __vectors[];
 static struct pseudodesc idt_pd = {
     sizeof(idt) - 1, (uintptr_t)idt
 };
@@ -48,6 +49,23 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+    extern uintptr_t __vectors[];
+    
+	//best code
+    uint32_t ind;
+    for (ind = 0; ind <= 255; ind++){
+    	if (ind == T_SYSCALL){
+	    SETGATE(idt[ind], 1, GD_KTEXT, __vectors[ind], DPL_USER);	
+	}
+	else if (ind < IRQ_OFFSET){
+	    SETGATE(idt[ind], 1, GD_KTEXT, __vectors[ind], DPL_KERNEL);	
+	}
+	else {
+	    SETGATE(idt[ind], 0, GD_KTEXT, __vectors[ind], DPL_KERNEL);	
+	}
+    }
+
+    lidt(&idt_pd);
 }
 
 static const char *
